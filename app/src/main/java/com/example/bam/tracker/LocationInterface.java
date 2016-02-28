b11010b5e07ead513651ff71f9f1d05ae17ef8c6
bhavesh4513 <bchalke4513@outlook.com>
2016-02-28 21:30:54 +0530

    Revert "project"
    This reverts commit 0ba4b4169302a900c32d9b9b8006d9d6ad99cff8.

--- app/src/main/java/com/example/bam/tracker/LocationInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.bam.tracker;
-
-import com.google.android.gms.maps.GoogleMap;
-
-public interface LocationInterface {
-    public void update(Double latitude,Double longitude);
-
-}
